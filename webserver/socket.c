3a1ae52aee4f429692d0552b35969093b11a152c
Maximilien DEROO <maximilien.deroo@etudiant.univ-lille1.fr>
2015-01-28 11:12:11 +0100

    Prout

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,6 +7,7 @@
 int creer_serveur(int port){
 	int socket_serveur;
 	int bind_serveur;
+
 	struct sockaddr_in sockaddr;
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_port = htons(port);
@@ -14,9 +15,10 @@ int creer_serveur(int port){
 	socket_serveur = socket (AF_INET, SOCK_STREAM, 0);
 	if(socket_serveur == -1)
 		perror("socket_serveur");
-	/* Utilisation de la socket serveur */
+
 	if((bind_serveur = bind(socket_serveur, (struct sockaddr *)&sockaddr, sizeof(sockaddr))) == -1)
 		perror("bind socker_serveur");
+
 	if(listen(socket_serveur, 10) == -1)
 		perror("Listen socket_serveur");
 	return socket_serveur;
