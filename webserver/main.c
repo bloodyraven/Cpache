f96748df2bc06414c77a7027ce675216c385e90d
Maximilien DEROO <maximilien.deroo@etudiant.univ-lille1.fr>
2015-03-23 12:32:20 +0100

    LOOOOOOOL

--- webserver/main.c
+++ webserver/main.c
@@ -1,21 +1,24 @@
 #include "socket.h"
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <signal.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(/*int argc, char ** argv*/ void)
 {
 	int pid;
 	int client;
 	char buffer[256];
-	char get[256];
- 	char path[512];
- 	int versionMaj = -1, versionMin = -1;
-	//const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur \n";
+	//char get[256];
+ 	//char path[512];
+ 	http_request request;
 	int serveur = creer_serveur(8080);
-	FILE* file = NULL;
+	FILE * file = NULL;
 
 	while(1) {
 		initialiser_signaux();
@@ -30,25 +33,19 @@ int main(/*int argc, char ** argv*/ void)
 					perror("close");
 			} else {
 				printf("Client connecté\n");
-				/*sleep(2);
-				if(write(client, message_bienvenue, strlen(message_bienvenue)) == -1)
-					perror("write");*/
 				file = fdopen(client,"w+");
-				/*ligneVide(buffer);
-				fgets(buffer, sizeof(buffer), file);
-				printf("%d\n", analyse(buffer, get, path, &versionMaj, &versionMin));*/
-	 			while(fgets(buffer, sizeof(buffer), file) != NULL) {
-					//fprintf(file, "<Cpache> %s", buffer);
-					if(analyse(buffer, get, path, &versionMaj, &versionMin) == -1) {
-						fprintf(file,"HTTP/1.1 404 Not found\r\nConnection: close\r\nContent-Length: 16\r\n");
-					} else {
-						fprintf(file,"HTTP/1.1 200 Good Request\r\nConnection: close\r\nContent-Length: 17\r\n");
-						fprintf(file,"Bienvenue sur Cpache ! \n");
-					}
+	 			fgets_or_exit(buffer, sizeof(buffer), file);
+				skip_headers(file);
+				if(parse_http_request(buffer, &request) == -1) {
+					fprintf(file,"HTTP/1.1 404 Not found\r\nConnection: close\r\nContent-Length: 16\r\n");
+				} else {
+					fprintf(file,"HTTP/1.1 200 Good Request\r\nConnection: close\r\nContent-Length: 17\r\n");
+					fprintf(file,"Bienvenue sur Cpache ! \n");
 				}
+			}
 				printf("Client deconnecté\n");
 				fclose(file);
-			}
+		
  		}
 	}
 	return 0;
--- webserver/socket.h
+++ webserver/socket.h
@@ -7,8 +7,27 @@ doit pouvoir être utilisée directement par un appel à accept .
 La fonction retourne -1 en cas d ’ erreur ou le descripteur de la
 socket créée . */
 
+enum http_method {
+	HTTP_GET,
+	HTTP_UNSUPPORTED
+};
+
+typedef struct
+{
+	enum http_method method;
+	int major_version;
+	int minor_version;
+	char * url;
+} http_request;
+
 int creer_serveur (int port);
 void initialiser_signaux(void);
 void ligneVide(char *str);
-int analyse(char buffer[], char *get, char *path, int *versionMaj, int *versionMin);
+
+int parse_http_request(const char *request_line, http_request *request);
+void skip_headers (FILE *fichier_client);
+void send_status(FILE * fichier_client, int code, const char * reason_phrase);
+void send_response(FILE * client, int code, const char *reason_phrase, const char *message_body);
+char * fgets_or_exit(char *buffer, int size, FILE *file);
+
 # endif
